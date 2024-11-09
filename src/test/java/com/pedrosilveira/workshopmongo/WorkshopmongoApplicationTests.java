0fa42194aa06663325638f51bb95215198d90ffc
Pedro Silveira <pedroadasilveirajr@hotmail.com>
2024-11-09 17:19:13 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/pedrosilveira/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrosilveira.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
